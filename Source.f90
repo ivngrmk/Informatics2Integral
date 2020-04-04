8e28fefd35662eb25ef0c01fbb7ca0f6fa32133a
Ivan Goremykin <ivngrmk@gmail.com>
2020-04-04 17:22:14 +0300

    Начало прикручивания графики. Создано окно, в нём нарисованы оси. Выполнение соответствующих построений происходит в двух подпраграммах, в которых инициализируются все необходимые для построений переменные.

--- Source.f90
+++ Source.f90
@@ -1,11 +1,13 @@
 program Integral
+    use dflib
     !ќбъ€вление переменных
     real, parameter :: pi = 3.1415926535
     integer N
     !*****************************************************************************************!
-    read(*,*) N
-    write(*,*) IntegralError(2.0)
-    
+    !read(*,*) N
+    !write(*,*) IntegralError(2.0)
+    call GraphicWindow()
+    call GraphicAxes()
     !*****************************************************************************************!
     contains
     real function f(xt)
@@ -41,5 +43,40 @@ program Integral
         end do
         IntegralError = Sum / 100.0
     end function IntegralError
+    !---------------------------------------------------------------------------------------!
+    subroutine GraphicWindow() !—оздаЄт окно дл€ вывода графики. ‘он - белый, граница - чЄрна€, само окно - белое.
+        logical(2) bool2
+        integer Pxl, Pxr, Pyu, Pyd
+        Pxl = 100; Pyl = 50; Pxr = 900; Pyr = 650
+        bool2 = SetBkColor(15); call ClearScreen(0) !ќкраска всего экрана в белый.
+        bool2 = SetColor(0); bool2 = Rectangle($GBORDER,Pxl-1, Pyl-1, Pxr+1, Pyr+1) !—оздание границ окна.
+        call SetViewPort(Pxl, Pyl, Pxr, Pyr) !—оздание рабочей области.
+        bool2 = SetBkColor(15); call ClearScreen(1) !ќкраска рабочей области под график в белый. 1 - —сылка на эту рабочую область.
+    end subroutine GraphicWindow
+    !---------------------------------------------------------------------------------------!
+    subroutine GraphicAxes()
+        real xl, yl, xr, yr, scale_width
+        real x, y
+        type (wxycoord) wxy
+        xl = -0.1; yl = -6.0; xr = 7.0; yr = 0.1; scale_width = 0.05 !ќб€зательно должны содержать начало координат
+        bool2 = SetWindow(.TRUE., DBLE(xl), DBLE(yl), DBLE(xr), DBLE(yr))
+        x = xl
+        do while (ceiling(x) <= floor(xr))
+            call MoveTo_w(DBLE(ceiling(x)), DBLE(0.0 - scale_width), wxy)
+            bool2 = LineTo_w(DBLE(ceiling(x)), DBLE(0.0 + scale_width))
+            x = x + 1
+        end do
+        y = yl
+        do while (ceiling(y) <= floor(yr))
+            call MoveTo_w(DBLE(0.0 - scale_width), DBLE(ceiling(y)), wxy)
+            bool2 = LineTo_w(DBLE(0.0 + scale_width), DBLE(ceiling(y)))
+            y = y + 1
+        end do
+        bool2 = SetColor(4) !–исуем сами оси
+        call MoveTo_w(DBLE(xl), DBLE(0.0), wxy)
+        bool2 = LineTo_w(DBLE(xr), DBLE(0.0))
+        call MoveTo_w(DBLE(0.0), DBLE(yl), wxy)
+        bool2 = LineTo_w(DBLE(0.0), DBLE(yr))
+    end subroutine GraphicAxes
 end
     
